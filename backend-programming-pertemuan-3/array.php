e01b4fbfbc97513d529855d1d76f17653a944fee
AgungSaputra <72749623+Agung-npc@users.noreply.github.com>
2021-11-23 08:54:42 +0700

    Delete backend-programming-pertemuan-3 directory

--- backend-programming-pertemuan-3/array.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-# membuat array
-$animals = ['cat', 'dog', 'fish'];
-
-# mengakses elemen array
-echo "$animals[0] <br>";
-echo "$animals[1] <br>";
--- backend-programming-pertemuan-3/looping-for.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-# looping for
-for ($i = 0; $i < 10; $i++) {
-  echo "Looping ke $i <br>";
-}

258a2d9e028f266e23628aabb4d62f4de578fc47
AgungSaputra <72749623+Agung-npc@users.noreply.github.com>
2022-01-28 09:39:39 +0700

    Update students.js

--- backend-programming-pertemuan-12/data/students.js
+++ backend-programming-pertemuan-12/data/students.js
@@ -1,5 +1,5 @@
-// TODO 1: Buat data students
+/* TODO 1: Buat data students */
 const students = ["Budi", "Dino", "Ucup", "Firman"];
 
-// TODO 2: export data students
+/* TODO 2: export data students */
 module.exports = students;
